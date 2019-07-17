a158e8526ae4ae53293ba891c52c441f7ceee88d
Andrea Taglia <andrea.taglia@pwc.com>
2019-07-18 00:27:23 +0200

    removed some stuff not usefull anymore

--- scraper.js
+++ scraper.js
@@ -1,102 +1,55 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
-const sites = ['runtastic', 'garmin', 'strava', 'facebook'];
-
-async function recognizeSite(url) {
-  for (var site of sites) {
-    if (url.indexOf(site) > -1) {
-      return site;
-    }
-  }
-  console.log("!!! url of unknown site: " + url);
-  return null;
-}
+const URL = 'https://en.wikipedia.org/wiki/List_of_countries_and_territories_by_land_and_maritime_borders'
 
 async function scrape(url) {
   const browser = await puppeteer.launch({
     headless: true,
     args: ['--account-consistency']
   });
-  try {
-    const page = await browser.newPage();
-    page.setViewport({
-      width: 1280,
-      height: 926
-    });
-    await page.goto(url, {
-      "waitUntil": "networkidle0"
-    }).catch(e => {
-      throw new Error(`"${e.message}"`);
-    });;
-    // await page.waitFor(1000);
-    const siteType = await recognizeSite(url);
-    var result;
-    if (siteType == 'facebook') {
-      //TODO login issues
-      // items.push('fb here');
-      // let elements = document.querySelectorAll('#js_20');
-      // for (var el of elements) {
-      //   console.log(el.innerText);
-      // }
-      result = 'fb result';
-    } else {
-      result = await page.evaluate(siteType => {
-        //data,km,ritmo,durata,dislivello positivo
-        const items = [];
-        try {
-          if (siteType == 'runtastic') {
-            //date
-            let elements = document.querySelectorAll('h2');
-            items.push(elements[1].innerText.replace(",", "."));
-            //other data
-            elements = document.querySelectorAll('h4');
-            items.push(elements[0].innerText.replace(",", "."));
-            items.push(elements[2].innerText);
-            items.push(elements[1].innerText);
-            items.push(elements[4].innerText);
-          } else if (siteType == 'garmin') {
-            //date
-            let elements = document.querySelectorAll('.page-feature div');
-            items.push(elements[0].innerText.slice(-24));
-            //other data
-            elements = document.querySelectorAll('.data-bit');
-            items.push(elements[1].innerText.replace(",", "."));
-            items.push(elements[3].innerText);
-            items.push(elements[2].innerText);
-            items.push(elements[4].innerText);
-          } else if (siteType == 'strava') {
-            //date
-            let elements = document.querySelectorAll('.timestamp');
-            items.push(elements[0].innerText.replace(",", " "));
-            //other data
-            elements = document.querySelectorAll('.inline-stats li strong');
-            items.push(elements[0].innerText);
-            items.push(elements[2].innerText);
-            items.push(elements[1].innerText);
-            //dislivello not available
-            items.push('0');
-          }
+  const page = await browser.newPage();
+  page.setViewport({
+    width: 1280,
+    height: 926
+  });
+  await page.goto(URL, {
+    "waitUntil": "networkidle0"
+  }).catch(e => {
+    throw new Error(`"${e.message}"`);
+  });;
+  await page.waitFor(1000);
+  var result;
 
-        } catch (error) {
-          console.log(error);
-          console.log("failed to scrape a url of type: " + siteType);
-          return 'failed';
-        }
+  result = await page.evaluate(() => {
+    //data,km,ritmo,durata,dislivello positivo
+    const items = [];
+    try {
+      //date
+      let elements = document.querySelectorAll('.timestamp');
+      items.push(elements[0].innerText.replace(",", " "));
+      //other data
+      elements = document.querySelectorAll('.inline-stats li strong');
+      items.push(elements[0].innerText);
+      items.push(elements[2].innerText);
+      items.push(elements[1].innerText);
+      //dislivello not available
+      items.push('0');
 
-        return items;
-      }, siteType);
+    } catch (error) {
+      console.log(error);
+      console.log("failed to scrape a url of type: " + siteType);
+      return 'failed';
+    } finally {
+
+      browser.close();
     }
 
-    browser.close();
-    return result;
-  } catch (error) {
-    browser.close();
-    throw new Error(`"${error.message}"`);
-  }
+    return items;
+  }, siteType);
 }
 
-// scrape('https://www.facebook.com/photo.php?fbid=1933392113338291&set=a.290544414289744.83087.100000025635366&type=3&theater').then((ret) => {
-//   console.log(ret);
-// });
+scrape('https://www.facebook.com/photo.php?fbid=1933392113338291&set=a.290544414289744.83087.100000025635366&type=3&theater').then((ret) => {
+  console.log(ret);
+});
 
 module.exports.scrape = scrape;
